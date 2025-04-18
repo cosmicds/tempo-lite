a0dbc742716bca8c17f08ff2976dfaae634ee979
John Arban Lewis <johnarban@users.noreply.github.com>
2025-04-18 12:23:18 -0400

    track which urls are looked at

--- src/TempoLite.vue
+++ src/TempoLite.vue
@@ -924,13 +924,54 @@ const fosterTimestamps = [
 // combine the timestamps from the two sources
 
 const timestamps = fosterTimestamps;
+const STORY_VISIT_URL = "";
+function pingServer(venue: string) {
+  // console.log(`pinging server with venue: ${JSON.stringify(venue)}`);
 
-
-
+  fetch(STORY_VISIT_URL, {
+    
+    method: "POST",
+    
+    headers: {
+      "Content-Type": "application/json",
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      "Authorization": process.env.VUE_APP_CDS_API_KEY ?? ''
+    },
+    
+    body: JSON.stringify({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      story_name: "tempo-lite",
+      "info": {"venue": venue}
+    })
+    
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Error pinging server", response.status, response.statusText);
+      } 
+    }).
+    catch((error) => {
+      console.error("Error pinging server", error);
+    });
+  
+  window.localStorage.setItem("venues", venue);
+}
 
 const urlParams = new URLSearchParams(window.location.search);
 const hideIntro = urlParams.get("hideintro") === "true";
 const WINDOW_DONTSHOWINTRO = hideIntro ? true: window.localStorage.getItem("dontShowIntro") === 'true';
+
+const venue = urlParams.get("venue");
+if (venue !== null) {
+  // get current value of venue
+  const venues =  window.localStorage.getItem("venues") ?? '';
+  // if they haven't visited this venue before, ping the server
+  if (!venues.includes(venue)) {
+    pingServer((venues ? venues  + ','  : '') + venue.replace(/,/g, '-'));
+  } else {
+    console.error("Venue is null or user has already visited");
+  }
+}
 
 const initLat = parseFloat(urlParams.get("lat") || '40.044');
 const initLon = parseFloat(urlParams.get("lon") || '-98.789');
